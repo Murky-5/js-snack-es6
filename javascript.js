3d830ae6dcf42851c052d13f85a97dd8cca032c9
MirkoDD <mirkodidio111@gmail.com>
2024-12-12 17:46:04 +0100

    added:commands(file)_and_created_teams

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 /* Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore.
-*/
+Stampare a schermo la bici con peso minore. */
 
 let bikes = [
     {
@@ -15,17 +14,43 @@ let bikes = [
 }
 ]
 
-console.log(bikes[0].weight > bikes[1].weight ? bikes[1] : bikes[0]) /* Basically (?) is the if? and the two dots (:) are the else? */
+console.log(bikes[0].weight > bikes[1].weight ? bikes[1] : bikes[0]) /* Basically the question mark (?) is the if? and the two dots (:) are the else? */
 
 
-/*
-Snack2
+/* Snack2
 Creare un array di oggetti di squadre di calcio.
- Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-Snack 3 (Bonus)
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+
+let inField = [
+    {
+        team: "narcelli",
+        scores: 0,
+        foul: 0,
+    },
+    {
+        team: "romia",
+        scores: 0,
+        foul: 0,
+    },
+    {
+        team: "casaleri",
+        scores: 0,
+        foul: 0,
+    },
+    {
+        team: "emceseli",
+        scores: 0,
+        foul: 0,
+    },
+]
+
+
+
+
+/* Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. */
-
